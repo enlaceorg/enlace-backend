7ee6c9dce00438e580eff1e17d68d48973f98dda
unknown <contact.guttler@gmail.com>
2022-05-06 09:12:46 -0300

    Model Usuario

---
+++ enlace/src/main/java/com/generation/enlace/model/UsuarioModel.java
@@ -0,0 +1,94 @@
+package com.generation.enlace.model;
+
+import java.time.Instant;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.data.annotation.CreatedDate;
+
+@Entity
+@Table(name="usuarios")
+public class UsuarioModel {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long usuario_id;
+	
+	@NotNull
+	@Size(min=1,max=100)
+	private String nome;
+	/*
+	@NotNull
+	@Size(min=1,max=100)
+	private String sobrenome;
+	*/
+	@NotNull
+	private String email;
+
+	@NotNull
+	private String senha;
+	
+	@NotNull
+	@Column(name = "imagem_url")
+	private String imagemUrl;
+
+	@NotNull
+	@CreatedDate
+	@Column(name = "criado_em")
+	private Instant criadoEm = Instant.now();
+
+	public Long getUsuario_id() {
+		return usuario_id;
+	}
+
+	public void setUsuario_id(Long usuario_id) {
+		this.usuario_id = usuario_id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getImagemUrl() {
+		return imagemUrl;
+	}
+
+	public void setImagemUrl(String imagemUrl) {
+		this.imagemUrl = imagemUrl;
+	}
+
+	public Instant getCriadoEm() {
+		return criadoEm;
+	}
+
+	public void setCriadoEm(Instant criadoEm) {
+		this.criadoEm = criadoEm;
+	}
+}

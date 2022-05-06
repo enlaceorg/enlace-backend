475c6448091fc67ed8c70aed1931c31b5c705b6a
Mayarabnc <mayarabncabral@gmail.com>
2022-05-06 10:45:51 -0300

    Adicionando postagem controller

--- enlace/src/main/java/com/generation/enlace/controller/PostagemController.java
+++ enlace/src/main/java/com/generation/enlace/controller/PostagemController.java
@@ -12,10 +12,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.generation.enlace.model.PostagemModel;
+import com.generation.enlace.repository.PostagemRepository;
 
 @RestController
 @RequestMapping("/postagem")
@@ -45,6 +48,12 @@ public class PostagemController {
 	@PostMapping
 	public ResponseEntity<PostagemModel> post(@Valid @RequestBody PostagemModel conteudo){
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(conteudo));		
+	}
+	
+	@PutMapping
+	public ResponseEntity<PostagemModel> put(@Valid @RequestBody PostagemModel conteudo){
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(conteudo));
+				
 	}
 	
 	@DeleteMapping("/{postagem_id}")
